746831c45a0e5b31c29725a4db90cffa4a1d73d9
Dani <gdschanu.hieund1601@gmail.com>
2024-09-17 00:23:31 +0700

    sum

---
+++ sum-2-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    int sum = num1 + num2;
+    printf("The sum of the two numbers is: %d\n", sum);
+    
+    return 0;
+}
